03ad2b7cdcd3aeeae81236cd09237f96bdfbbe85
Giliard <giliardpereirasobral@hotmail.com>
2019-01-12 20:34:39 -0300

    Inserindo os arquivos iniciais do projeto

---
+++ Programa _Animado/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Programa__Animado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Programa Animado");
+            Console.ReadKey();
+        }
+
+    }
+}
